4c5853347246aeb99452233824154b65d02a80b0
mironbkv-ops <miron.bkv@gmail.com>
2025-12-28 10:39:42 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,25 @@ window.addEventListener("load", () => {
     loader.style.display = "none";
   }, 800);
 });
+const toggle = document.getElementById('themeToggle');
+const body = document.body;
+
+// Загружаем сохранённую тему
+const savedTheme = localStorage.getItem('theme');
+if (savedTheme) {
+  body.classList.add(savedTheme);
+  toggle.textContent = savedTheme === 'dark' ? '☀️' : '🌙';
+} else {
+  body.classList.add('dark');
+}
+
+// Переключение
+toggle.addEventListener('click', () => {
+  body.classList.toggle('dark');
+  body.classList.toggle('light');
+
+  const theme = body.classList.contains('dark') ? 'dark' : 'light';
+  localStorage.setItem('theme', theme);
+
+  toggle.textContent = theme === 'dark' ? '☀️' : '🌙';
+});
